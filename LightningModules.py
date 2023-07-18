60e1ca015040efc0dec7656ec3e53ff67aa208dc
Ishan Gaur <ishangaur4@gmail.com>
2023-07-18 22:28:35 +0200

    fucci prediction training pipeline memory error for prediction metrics

--- LightningModules.py
+++ LightningModules.py
@@ -9,16 +9,9 @@ from torch import nn
 from torch.nn import functional as F
 from torch.utils.data import DataLoader, random_split
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-from torchvision.utils import make_grid
-
 import lightning.pytorch as pl
-from lightning.pytorch.callbacks import Callback
-
-from kornia import tensor_to_image
-from kornia.losses import ssim_loss
-from microfilm.colorify import multichannel_to_rgb
 import numpy as np
-import wandb
+
 
 from FUCCIDataset import FUCCIDataset, ReferenceChannelDataset, FUCCIChannelDataset
 from FUCCIDataset import FUCCIDatasetInMemory, ReferenceChannelDatasetInMemory, FUCCIChannelDatasetInMemory, TotalDatasetInMemory
@@ -123,9 +116,9 @@ class FUCCIModel(pl.LightningModule):
         self.maps_out = nn.ModuleList()
         self.channels_in = ["DAPI", "gamma-tubulin"]
         self.channels_out = ["Geminin", "CDT1"]
-        for channel in self.channels_in:
+        for _ in self.channels_in:
             self.maps_in.append(torch.compile(MapperIn(self.ae_latent, self.map_widths)))
-        for channel in self.channels_out:
+        for _ in self.channels_out:
             self.maps_out.append(torch.compile(MapperOut(self.ae_latent, self.map_widths)))
 
         self.latent_dim = latent_dim * self.map_widths[-1]
@@ -137,7 +130,7 @@ class FUCCIModel(pl.LightningModule):
 
     def encode(self, x): # x is B x C(2) x H x W
         # predicted variance for the autoencoder checkpoint is 0
-        image_mu, _ = self.encoder(x.view(-1, x.shape[-2], x.shape[-1]))
+        image_mu, _ = self.encoder(x.reshape(-1, 1, x.shape[-2], x.shape[-1]))
         image_z = image_mu # B x latent_dim, channels are interleaved
         mus, logvars = [], []
         for c, map_in in enumerate(self.maps_in):
@@ -150,7 +143,7 @@ class FUCCIModel(pl.LightningModule):
         mus = torch.stack(mus, dim=1) # B x c x latent_dim
         mu = mus.mean(dim=1) # B x latent_dim
         vars = torch.stack(logvars, dim=1).exp()
-        var = (vars + mu.pow(2)).mean(dim=1) - mu.pow(2).mean(dim=1)
+        var = (vars + mus.pow(2)).mean(dim=1) - mus.mean(dim=1).pow(2)
         logvar = var.log()
         return mu, logvar
     
@@ -160,12 +153,12 @@ class FUCCIModel(pl.LightningModule):
             image_zs.append(map_out(z)[:, None, ...])
         image_zs = torch.cat(image_zs, dim=1)
         image = self.decoder(image_zs.view(-1, image_zs.shape[-2], image_zs.shape[-1]))
-        image = image.reshape(image_zs.shape[0], len(self.channels_out), image_zs.shape[-2], image_zs.shape[-1])
+        image = image.reshape(image_zs.shape[0], len(self.channels_out), image.shape[-2], image.shape[-1])
         return image
 
     def forward(self, x):
         # x is N x 4 x 256 x 256 (N four-channel images)
-        x = x[:, :2]
+        # x = x[:, :2]
         mus, logvars = self.encode(x)
         mu_z, logvar_z = self.sample_average(mus, logvars)
         z = self.reparameterized_sampling(mu_z, logvar_z)
@@ -186,7 +179,6 @@ class FUCCIModel(pl.LightningModule):
         # return torch.sum(logvar) + torch.sum(torch.exp(-logvar + self.eps)) + torch.sum(mu.pow(2))
 
     def sigma_reconstruction_loss(self, x, x_hat):
-        x = x[:, 2:] # comparing only to the target channels
         log_sigma = ((x_hat - x) ** 2).mean(list(range(len(x.shape))), keepdim=True).sqrt().log()
         min = np.log(self.eps) # just above e^-20 so min is -20
         log_sigma = min + F.softplus(log_sigma - min)
@@ -224,9 +216,10 @@ class FUCCIModel(pl.LightningModule):
         for param in self.decoder.parameters():
             param.requires_grad = False
         parameters = []
-        for i in range(len(self.channels)):
-            parameters.extend(self.maps_in[i].parameters())
-            parameters.extend(self.maps_out[i].parameters())
+        for c in range(len(self.channels_in)):
+            parameters.extend(self.maps_in[c].parameters())
+        for c in range(len(self.channels_out)):
+            parameters.extend(self.maps_out[c].parameters())
         optimizer = optim.Adam(parameters, lr=self.lr)
         return optimizer
 
@@ -765,246 +758,3 @@ class AutoEncoder(pl.LightningModule):
     def lr_scheduler_step(self, scheduler, metric):
         scheduler.step(metric)
         # self.log("lr", scheduler._last_lr[0], on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
-
-
-class ReconstructionVisualization(Callback):
-    def __init__(self, num_images=8, every_n_epochs=5, channels=None, mode="single"):
-        super().__init__()
-        self.num_images = num_images
-        self.every_n_epochs = every_n_epochs
-        self.channels = channels
-        self.mode = mode
-
-    def __shared_reconstruction_step(self, input_imgs, pl_module, cmap):
-        input_imgs = input_imgs.to(pl_module.device)
-        with torch.no_grad():
-            pl_module.eval()
-            reconst_imgs = pl_module(input_imgs)
-            pl_module.train()
-        grid = ReconstructionVisualization.make_reconstruction_grid(input_imgs, reconst_imgs)
-        grid = tensor_to_image(grid)
-        grid = np.moveaxis(grid, -1, 0)
-        rgb_grid, _, _, _ = multichannel_to_rgb(grid, cmaps=cmap)
-        return rgb_grid, grid
-
-    def __perm_reconstruction_step(self, batch, pl_module, cmap):
-        batch = torch.clone(batch)
-        batch = batch.to(pl_module.device)
-        with torch.no_grad():
-            pl_module.eval()
-            embeddings = {channel: None for channel in range(len(batch))} # because you can't directly access the channel list here
-            for channel, channel_batch in enumerate(batch):
-                mu, logvar = pl_module.encode(channel_batch, channel)
-                embeddings[channel] = pl_module.reparameterized_sampling(mu, logvar)
-            embeddings = [embeddings[channel] for channel in embeddings]
-            embeddings = torch.stack(embeddings)
-            loss, x_hat, x = pl_module.perm_mse_loss(embeddings, batch)
-            pl_module.train()
-        x = x.transpose(0, 1)
-        x_hat = x_hat.transpose(0, 1)
-        x = x.squeeze(2)
-        x_hat = x_hat.squeeze(2)
-        grid = ReconstructionVisualization.make_reconstruction_grid(x, x_hat)
-        grid = tensor_to_image(grid)
-        grid = np.moveaxis(grid, -1, 0)
-        rgb_grid, _, _, _ = multichannel_to_rgb(grid, cmaps=cmap)
-        return rgb_grid, grid
-
-    def __one_to_all_reconstruction_step(self, batch, pl_module, cmap):
-        batch = torch.clone(batch)
-        batch = batch.to(pl_module.device) # 4 x N x 1 x H x W
-        batch.transpose_(0, 1) # N x 4 x 1 x H x W
-        batch = batch[:batch.shape[1]] # one prediction per channel
-        batch.transpose_(0, 1) # 4 x N(4) x 1 x H x W
-        with torch.no_grad():
-            pl_module.eval()
-            embeddings = {channel: None for channel in range(len(batch))}
-            for channel, channel_batch in enumerate(batch):
-                mu, logvar = pl_module.encode(channel_batch, channel)
-                embeddings[channel] = pl_module.reparameterized_sampling(mu, logvar)
-            embeddings = [embeddings[channel] for channel in embeddings]
-            embeddings = torch.stack(embeddings) # 4 x N(4) x 512
-            embeddings.transpose_(0, 1) # N x 4 x 512
-            batch.transpose_(0, 1) # N x 4 x 1 x H x W
-            x_hat = torch.zeros_like(batch)
-            for channel, image_embed in enumerate(embeddings):
-                for output_channel in range(batch.shape[1]):
-                    x_hat[channel, output_channel] = pl_module.decode(image_embed[channel, None, ...], output_channel)
-            pl_module.train()
-        x = batch.squeeze(2)
-        x_hat = x_hat.squeeze(2)
-        grid = ReconstructionVisualization.make_reconstruction_grid(x, x_hat)
-        grid = tensor_to_image(grid)
-        grid = np.moveaxis(grid, -1, 0)
-        rgb_grid, _, _, _ = multichannel_to_rgb(grid, cmaps=cmap)
-        return rgb_grid, grid
-
-    def __many_to_one_reconstruction_step(self, batch, pl_module, cmap):
-        batch = torch.clone(batch)
-        batch = batch.to(pl_module.device) # 4 x N x 1 x H x W
-        batch.transpose_(0, 1) # N x 4 x 1 x H x W
-        batch = batch[:batch.shape[1]] # one prediction per channel
-        batch.transpose_(0, 1) # 4 x N(4) x 1 x H x W
-        with torch.no_grad():
-            pl_module.eval()
-
-            # get embeddings per channel in the batch (each one has all images)
-            embeddings = {channel: None for channel in range(len(batch))}
-            for channel, channel_batch in enumerate(batch):
-                mu, logvar = pl_module.encode(channel_batch, channel)
-                embeddings[channel] = pl_module.reparameterized_sampling(mu, logvar)
-            embeddings = [embeddings[channel] for channel in embeddings]
-            embeddings = torch.stack(embeddings) # 4 x N(4) x 512
-            embeddings.transpose_(0, 1) # N x 4 x 512
-            batch.transpose_(0, 1) # N x 4 x 1 x H x W
-
-            # inference by using all embeddings other than the channel of interest to predict the channel
-            x_hat = torch.zeros_like(batch)
-            for channel, image_embed in enumerate(embeddings):
-                for output_channel in range(batch.shape[1]):
-                    if channel != output_channel:
-                        x_hat[channel, output_channel] = batch[channel, output_channel]
-                    else:
-                        embed_pred = torch.zeros_like(image_embed[0])
-                        for c in range(batch.shape[1]):
-                            if c != channel:
-                                embed_pred += image_embed[c]
-                        embed_pred /= (batch.shape[1] - 1)
-                        x_hat[channel, output_channel] = pl_module.decode(embed_pred, output_channel)
-            pl_module.train()
-        x = batch.squeeze(2)
-        x_hat = x_hat.squeeze(2)
-        grid = ReconstructionVisualization.make_reconstruction_grid(x, x_hat)
-        grid = tensor_to_image(grid)
-        grid = np.moveaxis(grid, -1, 0)
-        rgb_grid, _, _, _ = multichannel_to_rgb(grid, cmaps=cmap)
-        return rgb_grid, grid
-
-    @staticmethod
-    def image_list_normalization(image_list):
-        for i in range(len(image_list)):
-            image_list[i] = image_list[i] - torch.min(image_list[i])
-            image_list[i] = image_list[i] / (torch.max(image_list[i]) - torch.min(image_list[i]))
-            image_list[i] = image_list[i] * 2 - 1
-        return image_list
-
-    @staticmethod
-    def make_reconstruction_grid(input_imgs, reconst_imgs):
-        imgs = torch.stack([input_imgs, reconst_imgs], dim=1).flatten(0, 1)
-        grid = make_grid(imgs, nrow=2, normalize=True)
-        return grid
-
-    def __shared_logging_step(self, input_imgs, pl_module, cmap, trainer, channel=""):
-        rgb_grid, grid = self.__shared_reconstruction_step(input_imgs, pl_module, cmap)
-        if self.mode != "multi":
-            trainer.logger.experiment.log({
-                f"{trainer.state.stage}/reconstruction_samples": wandb.Image(rgb_grid,
-                    caption="Original and reconstructed images")
-            })
-
-        if self.channels is not None:
-            if len(self.channels) != grid.shape[0]:
-                raise ValueError(f"Number of channels ({len(self.channels)}) does not match number of images ({grid.shape[0]})")
-            for i, channel in enumerate(self.channels):
-                trainer.logger.experiment.log({
-                    f"{trainer.state.stage}/reconstruction_samples_{channel}": wandb.Image(grid[i],
-                        caption=f"Original and reconstructed {channel} images")
-                })
-
-        if self.mode == "perm":
-            input_imgs = input_imgs.transpose(0, 1) # 4 x N x 256 x 256
-            input_imgs = input_imgs[:, :, None, ...] # 4 x N x 1 x 256 x 256
-            # rgb_grid, grid = self.__perm_reconstruction_step(input_imgs, pl_module, cmap)
-            rgb_grid, grid = self.__perm_reconstruction_step(input_imgs, pl_module, cmap)
-            trainer.logger.experiment.log({
-                f"{trainer.state.stage}/perm_reconstruction_samples": wandb.Image(rgb_grid,
-                    caption="Original and reconstructed images")
-            })
-            rgb_grid, grid = self.__one_to_all_reconstruction_step(input_imgs, pl_module, cmap)
-            trainer.logger.experiment.log({
-                f"{trainer.state.stage}/one_to_many_reconstruction_samples": wandb.Image(rgb_grid,
-                    caption="Original and reconstructed images")
-            })
-            rgb_grid, grid = self.__many_to_one_reconstruction_step(input_imgs, pl_module, cmap)
-            trainer.logger.experiment.log({
-                f"{trainer.state.stage}/many_to_one_reconstruction_samples": wandb.Image(rgb_grid,
-                    caption="Original and reconstructed images")
-            })
-
-    def __shared_logging_dispatch(self, dataloader, pl_module, trainer):
-        if self.mode == "multi":
-            for channel, data in dataloader.iterables.items():
-                input_imgs = data[:self.num_images]
-                cmap = None
-                self.__shared_logging_step(input_imgs, pl_module, cmap, trainer, channel)
-        else:
-            input_imgs = dataloader.dataset[:self.num_images]
-            cmap = trainer.datamodule.channel_colors() if input_imgs.shape[-3] > 1 else None
-            self.__shared_logging_step(input_imgs, pl_module, cmap, trainer)
-
-    def on_train_epoch_end(self, trainer, pl_module):
-        if trainer.current_epoch % self.every_n_epochs == 0:
-            self.__shared_logging_dispatch(trainer.datamodule.train_dataloader(), pl_module, trainer)
-
-    def on_validation_epoch_end(self, trainer, pl_module):
-        if trainer.current_epoch % self.every_n_epochs == 0:
-            self.__shared_logging_dispatch(trainer.datamodule.val_dataloader(), pl_module, trainer)
-    def on_test_end(self, trainer, pl_module):
-        self.__shared_logging_dispatch(trainer.datamodule.test_dataloader(), pl_module, trainer)
-
-class EmbeddingLogger(Callback):
-    def __init__(self, num_images=200, every_n_epochs=5, mode="single", channels=[]):
-        super().__init__()
-        self.num_images = num_images
-        self.every_n_epochs = every_n_epochs
-        self.mode = mode
-        self.channels = channels
-
-    def __x_logging_step(self, x, trainer, name, channel=""):
-        trainer.logger.experiment.log({
-            f"{trainer.state.stage}/{channel}_embedding_{name}_mean": x.mean()
-        })
-        trainer.logger.experiment.log({
-            f"{trainer.state.stage}/{channel}_embedding_{name}_min": x.min()
-        })
-        trainer.logger.experiment.log({
-            f"{trainer.state.stage}/{channel}_embedding_{name}_max": x.max()
-        })
-
-    def __shared_logging_step(self, input_imgs, pl_module, trainer, channel=""):
-        input_imgs = input_imgs.to(pl_module.device)
-        with torch.no_grad():
-            pl_module.eval()
-            mu, logvar = pl_module.forward_embedding(input_imgs)
-            pl_module.train()
-        return mu, logvar
-
-    def __shared_logging_dispatch(self, dataloader, pl_module, trainer):
-        if self.mode == "multi":
-            for channel, data in dataloader.iterables.items():
-                input_imgs = data[:self.num_images]
-                mu, logvar = self.__shared_logging_step(input_imgs, pl_module, trainer, channel)
-                self.__x_logging_step(mu, trainer, "mu", channel)
-                self.__x_logging_step(logvar, trainer, "logvar", channel)
-        elif self.mode == "all":
-            input_imgs = dataloader.dataset[:self.num_images]
-            mu, logvar = self.__shared_logging_step(input_imgs, pl_module, trainer)
-            for m, l, c in zip(mu, logvar, self.channels):
-                self.__x_logging_step(m, trainer, "mu", c)
-                self.__x_logging_step(l, trainer, "logvar", c)
-        else:
-            input_imgs = dataloader.dataset[:self.num_images]
-            mu, logvar = self.__shared_logging_step(input_imgs, pl_module, trainer)
-            self.__x_logging_step(mu, trainer, "mu")
-            self.__x_logging_step(logvar, trainer, "logvar")
-
-    def on_train_epoch_end(self, trainer, pl_module):
-        if trainer.current_epoch % self.every_n_epochs == 0:
-            self.__shared_logging_dispatch(trainer.datamodule.train_dataloader(), pl_module, trainer)
-
-    def on_validation_epoch_end(self, trainer, pl_module):
-        if trainer.current_epoch % self.every_n_epochs == 0:
-            self.__shared_logging_dispatch(trainer.datamodule.val_dataloader(), pl_module, trainer)
-            
-    def on_test_end(self, trainer, pl_module):
-        self.__shared_logging_dispatch(trainer.datamodule.test_dataloader(), pl_module, trainer)
---
+++ fucci-trainer.py
@@ -0,0 +1,121 @@
+import os
+import time
+from pathlib import Path
+import argparse
+
+import torch
+import lightning.pytorch as pl
+from lightning.pytorch.loggers import WandbLogger
+from lightning.pytorch.callbacks import ModelCheckpoint
+
+from LightningModules import FUCCIModel, FUCCIDataModule
+from Metrics import ReconstructionVisualization, EmbeddingLogger, FUCCIPredictionLogger
+
+chkpt = Path("/data/ishang/fucci_vae/FUCCI_total_VAE_2023_06_28_07_08/lightning_logs/277-963202.12.ckpt")
+
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:1024"
+torch.set_float32_matmul_precision('medium')
+
+parser = argparse.ArgumentParser(description="Train a model to predict FUCCI channels from reference.",
+                                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-d", "--data", required=True, help="path to dataset")
+parser.add_argument("-c", "--cpu", action="store_true", help="run on CPU")
+parser.add_argument("-e", "--epochs", type=int, default=100, help="maximum number of epochs to train for")
+parser.add_argument("-m", "--checkpoint", help="path to checkpoint to load from")
+
+args = parser.parse_args()
+
+if args.checkpoint is not None:
+    if not Path(args.checkpoint).exists():
+        raise ValueError("Checkpoint path does not exist.")
+    
+config = {
+    "imsize": 256,
+    "latent_dim": 512,
+    "batch_size": 8,
+    "devices": [7],
+    # "devices": list(range(0, 4)),
+    # "devices": list(range(4, torch.cuda.device_count())),
+    # "devices": list(range(0, torch.cuda.device_count())),
+    # "devices": [3, 4, 5, 6, 7],
+    # "num_workers": 1,
+    "num_workers": 1,
+    # "num_workers": 5,
+    # "num_workers": 8,
+    "split": (0.64, 0.16, 0.2),
+    "lr": 5e-5,
+    "grad_clip": 1e3,
+    # "eps": 1e-12,
+    "epochs": args.epochs,
+    "mapper_mults": (2, 2)
+}
+
+def print_with_time(msg):
+    print(f"[{time.strftime('%m/%d/%Y @ %H:%M')}] {msg}")
+
+print_with_time("Setting up run file folders...")
+fucci_path = Path(args.data)
+project_name = f"FUCCI_fucci_pred_VAE"
+log_folder = Path(f"/data/ishang/fucci_vae/{project_name}_{time.strftime('%Y_%m_%d_%H_%M')}")
+if not log_folder.exists():
+    os.makedirs(log_folder, exist_ok=True)
+lightning_dir = log_folder / "lightning_logs"
+wandb_dir = log_folder
+
+
+print_with_time("Setting up data module...")
+dm = FUCCIDataModule(
+    data_dir=fucci_path,
+    dataset="all",
+    imsize=config["imsize"],
+    split=config["split"],
+    batch_size=config["batch_size"],
+    num_workers=config["num_workers"]
+)
+
+print_with_time("Setting up model...")
+model = FUCCIModel(
+    ae_checkpoint=chkpt,
+    latent_dim=config["latent_dim"],
+    lr=config["lr"],
+    map_widths=config["mapper_mults"]
+)
+
+wandb_logger = WandbLogger(
+    project=project_name,
+    log_model=True,
+    save_dir=wandb_dir,
+    config=config
+)
+
+wandb_logger.watch(model, log="all", log_freq=10)
+
+val_checkpoint_callback = ModelCheckpoint(
+    save_top_k=1,
+    monitor="validate/loss",
+    mode="min",
+    dirpath=lightning_dir,
+    filename="{epoch:02d}-{validate/loss:.2f}",
+    auto_insert_metric_name=False,
+)
+
+latest_checkpoint_callback = ModelCheckpoint(dirpath=lightning_dir, save_last=True)
+
+trainer = pl.Trainer(
+    default_root_dir=lightning_dir,
+    accelerator="gpu" if not args.cpu else "cpu",
+    devices=config["devices"] if not args.cpu else "auto",
+    strategy='ddp_find_unused_parameters_true',
+    logger=wandb_logger,
+    max_epochs=config["epochs"],
+    gradient_clip_val=config["grad_clip"],
+    callbacks=[
+        val_checkpoint_callback,
+        latest_checkpoint_callback,
+        ReconstructionVisualization(channels=None, mode="fucci", every_n_epochs=1),
+        # FUCCIPredictionLogger(every_n_epochs=1),
+    ]
+)
+
+print_with_time("Training model...")
+trainer.fit(model, dm)
