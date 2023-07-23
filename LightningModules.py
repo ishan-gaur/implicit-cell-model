augmentation, lr warmup, correcting order of fucci metric calculation

LightningModules.py
@@ -8,9 +8,11 @@ from torch import optim
 from torch import nn
 from torch.nn import functional as F
 from torch.utils.data import DataLoader, random_split
-from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.optim.lr_scheduler import ReduceLROnPlateau, LinearLR
 import lightning.pytorch as pl
 import numpy as np
+
+from kornia.augmentation import RandomRotation, RandomGaussianBlur, RandomSharpness
 
 
 from FUCCIDataset import FUCCIDataset, ReferenceChannelDataset, FUCCIChannelDataset
@@ -58,11 +60,7 @@ class FUCCIDataModule(pl.LightningDataModule):
         if len(self.split) != 3:
             raise ValueError("split must be a tuple of length 3")
         if deterministic:
-<<<<<<< HEAD
             generator = torch.Generator().manual_seed(420)
-=======
-            generator = torch.Generator().manual_seed(42)
->>>>>>> pretrained model for fucci prediction and deterministic splits
             self.data_train, self.data_val, self.data_test = random_split(self.dataset, self.split, generator=generator)
         else:
             self.data_train, self.data_val, self.data_test = random_split(self.dataset, self.split)
@@ -92,6 +90,31 @@ class FUCCIDataModule(pl.LightningDataModule):
         return self.dataset.channel_colors()
 
 
+class Bundle(nn.Module):
+    def __init__(self, maps: List[Tuple[nn.Module, nn.Module]]):
+        super().__init__()
+        self.maps = maps
+
+
+class DataAugmentation(nn.Module):
+    """Module to perform data augmentation using Kornia on torch tensors."""
+
+    def __init__(self, apply_color_jitter: bool = False) -> None:
+        super().__init__()
+        self._apply_color_jitter = apply_color_jitter
+
+        self.transforms = nn.Sequential(
+            RandomRotation(degrees=180.0, p=1.0),
+            RandomGaussianBlur(kernel_size=(3, 3), sigma=(0.0, 1.0), p=0.5),
+            RandomSharpness(p=0.5),
+        )
+
+    @torch.no_grad()  # disable gradients for effiency
+    def forward(self, x):
+        x_out = self.transforms(x)  # BxCxHxW
+        return x_out
+
+
 class FUCCIModel(pl.LightningModule):
     def __init__(self,
         ae_checkpoint: Path,
@@ -105,6 +128,8 @@ class FUCCIModel(pl.LightningModule):
         train_all: bool = False,
         train_encoder: bool = False,
         train_decoder: bool = False,
+        warmup: bool = False,
+        augmentation: bool = False
     ):
 
         super().__init__()
@@ -133,11 +158,21 @@ class FUCCIModel(pl.LightningModule):
         self.train_all = train_all
         self.train_encoder = train_encoder
         self.train_decoder = train_decoder
+        self.warmup = warmup
+        self.augmentation = augmentation
+
+        self.transform = DataAugmentation()
 
     def reparameterized_sampling(self, mu, logvar):
         std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
         return eps.mul(std).add_(mu)
+
+    def on_after_batch_transfer(self, batch, dataloader_idx):
+        x = batch
+        if self.trainer.training and self.augmentation:
+            x = self.transform(x)
+        return x
 
     def encode(self, x): # x is B x C(2) x H x W
         # predicted variance for the autoencoder checkpoint is 0
@@ -241,7 +276,19 @@ class FUCCIModel(pl.LightningModule):
             print("\tTraining decoder")
         optimizer = optim.Adam(parameters, lr=self.lr)
 
+        if self.warmup and (self.train_all or self.train_encoder or self.train_decoder):
+            print("\tUsing Linear Warmup from 0.01 to 1 over 10 epochs")
+            scheduler = LinearLR(optimizer, start_factor=1e-2, end_factor=1, total_iters=10)
+            return {
+                "optimizer": optimizer,
+                "lr_scheduler": scheduler,
+            }
+
         return optimizer
+
+    def lr_scheduler_step(self, scheduler, metric):
+        scheduler.step(metric)
+        self.logger.experiment.log({"lr": scheduler.get_last_lr()[0]})
 
     def training_step(self, batch, batch_idx):
         loss = self.__shared_step(batch, "train")

Metrics.py
@@ -290,7 +290,7 @@ class FUCCIPredictionLogger(Callback):
 
     def __fucci_level_from_image(self, img):
         # TODO: this is a problem if the denominator is small although this only happens for mitotic cells
-        return torch.sum(img[:, 1] - img[:, 0], dim=(1,2)) / torch.sum(img, dim=(1,2,3))
+        return torch.sum(img[:, 0] - img[:, 1], dim=(1,2)) / torch.sum(img, dim=(1,2,3))
 
     def __fucci_states_from_level(self, level):
         with_G2 = torch.where(level > 0.5, 2 * torch.ones_like(level), torch.zeros_like(level))

fucci-trainer.py
@@ -6,7 +6,7 @@ import argparse
 import torch
 import lightning.pytorch as pl
 from lightning.pytorch.loggers import WandbLogger
-from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch.callbacks import ModelCheckpoint, LearningRateMonitor
 
 from LightningModules import FUCCIModel, FUCCIDataModule
 from Metrics import ReconstructionVisualization, EmbeddingLogger, FUCCIPredictionLogger
@@ -19,12 +19,14 @@ torch.set_float32_matmul_precision('medium')
 parser = argparse.ArgumentParser(description="Train a model to predict FUCCI channels from reference.",
                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-d", "--data", required=True, help="path to dataset")
+parser.add_argument("-r", "--augment", action="store_true", help="augment the dataset (random rotations)")
 parser.add_argument("-e", "--epochs", type=int, default=100, help="maximum number of epochs to train for")
 parser.add_argument("-m", "--checkpoint", help="path to checkpoint for common pretrained autoencoder")
 parser.add_argument("-p", "--pretrained", help="path to pretrained model to load from")
 parser.add_argument("-a", "--all", action="store_true", help="train all of the model parameters")
 parser.add_argument("-f", "--featurizer", action="store_true", help="train the featurizer model as well")
 parser.add_argument("-g", "--generation", action="store_true", help="train the generation model as well")
+parser.add_argument("-w", "--warmup", action="store_true", help="warmup the model learning rate over 10 epochs")
 parser.add_argument("-c", "--cpu", action="store_true", help="run on CPU")
 
 args = parser.parse_args()
@@ -42,19 +44,21 @@ config = {
     # "devices": [7],
     # "devices": list(range(0, 4)),
     # "devices": list(range(4, torch.cuda.device_count())),
-    "devices": list(range(0, torch.cuda.device_count())),
-    # "devices": list(range(1, torch.cuda.device_count())),
-    # "devices": [3, 4, 5, 6, 7],
+    # "devices": list(range(0, torch.cuda.device_count())),
+    "devices": list(range(2, torch.cuda.device_count())),
+    # "devices": [2, 3, 4, 5, 6, 7],
     "num_workers": 1,
     # "num_workers": 4,
     # "num_workers": 5,
     # "num_workers": 8,
-    "split": (0.64, 0.16, 0.2),
-    "lr": 5e-10,
+    "split": (0.80, 0.10, 0.10),
+    "lr": 5e-5,
     "grad_clip": 1e3,
     # "eps": 1e-12,
     "epochs": args.epochs,
-    "mapper_mults": (2, 2)
+    "mapper_mults": (1, 1),
+    "warmup": args.warmup,
+    "augmentation": args.augment,
 }
 
 def print_with_time(msg):
@@ -86,6 +90,7 @@ if args.pretrained is not None:
     model.train_all = args.all
     model.train_generation = args.generation
     model.lr = config["lr"]
+    model.warmup = config["warmup"]
 else:
     model = FUCCIModel(
         ae_checkpoint=chkpt,
@@ -94,7 +99,8 @@ else:
         map_widths=config["mapper_mults"],
         train_all=args.all,
         train_encoder=args.featurizer,
-        train_decoder=args.generation
+        train_decoder=args.generation,
+        warmup=config["warmup"]
     )
 
 wandb_logger = WandbLogger(
@@ -129,6 +135,7 @@ trainer = pl.Trainer(
     callbacks=[
         val_checkpoint_callback,
         latest_checkpoint_callback,
+        LearningRateMonitor(logging_interval='step'),
         ReconstructionVisualization(channels=None, mode="fucci", every_n_epochs=1),
         FUCCIPredictionLogger(every_n_epochs=1),
     ]